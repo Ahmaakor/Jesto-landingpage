74c5ddfe67d08cd5f273280620ab3befe1b55557
Ahmad Abduljeleel <ahmadakorede04@gmail.com>
2024-11-10 18:14:06 +0100

    first commit

--- src/components/demoContent/Epic.js
+++ src/components/demoContent/Epic.js
@@ -1,15 +1,44 @@
 import { Link } from 'react-router-dom';    
+import { useRef, useState } from 'react';
 
 function Epic() {
+    const videoRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const handlePlayPause = () => {
+        if (isPlaying) {
+            videoRef.current.pause();
+        } else {
+            videoRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    };
+
     return (
         <>
-        <div className="epic">
-            <div className="new-content">
-                <h1>Start Something Epic</h1>
-                <p>Connect with jesters who share your sense of humor. Join JestoBook today and discover your perfect comedic match.</p>
-                <Link to="/auth" className="button button-primary">Create Account</Link>
+            <div className="epic">
+                <div className="new-content">
+                    <h1>Start Something Epic</h1>
+                    <p>Connect with jesters who share your sense of humor. Join JestoBook today and discover your perfect comedic match.</p>
+                    <Link to="/auth" className="button button-primary">Create Account</Link>
+                </div>
+                
+                <div className="video">
+                    <video 
+                        ref={videoRef}
+                        muted
+                        loop
+                        playsInline
+                        controls
+                        className="video-iframe"
+                        onClick={handlePlayPause}
+                    >
+                        <source src="/videos/video.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    
+                </div>
             </div>
-        </div>
         </>
     );
 }
--- src/components/demoContent/WhyChoose.js
+++ src/components/demoContent/WhyChoose.js
@@ -11,6 +11,38 @@ function WhyChoose() {
                     </div>
                 </div>
             </div>
+            <div className="container">
+                <div className="users">
+                    <div className="userpics">
+                        <img src='https://i.pinimg.com/236x/4c/34/f3/4c34f3cc3fccf59b678e2b3b2ec99a14.jpg' alt='pics'/>
+                    </div>
+                    <div className="userpics">
+                        <img src='https://i.pinimg.com/236x/f6/10/fe/f610feb7dda0168bb968a8830fd16b9c.jpg' alt='pics'/>
+                    </div>
+                    <div className="userpics">
+                        <img src='https://i.pinimg.com/236x/39/b9/9d/39b99d314b58bae6ab1065b1519220b1.jpg' alt='pics'/>
+                    </div>
+                    <div className="userpics">
+                        <img src='https://i.pinimg.com/236x/25/ab/b4/25abb44ae7e73bb0e54977b8340ba5a1.jpg' alt='pics'/>
+                    </div>
+                    <div className="userpics">
+                        <img src='https://i.pinimg.com/236x/69/6f/da/696fda26362eaa1a276f04b8a2e15af4.jpg' alt='pics'/>
+                    </div>
+                    <div className="userpics">
+                        <img src='https://i.pinimg.com/236x/0f/67/b6/0f67b66951c6c93087a254215420e438.jpg' alt='pics'/>
+                    </div>
+                    <div className="userpics">
+                        <img src='https://i.pinimg.com/236x/0c/d5/64/0cd564aa992f28f1b7b3ffd60fb05d04.jpg' alt='pics'/>
+                    </div>
+                    <div className="userpics">
+                        <img src='https://i.pinimg.com/236x/e4/41/54/e44154308e3466d987665c6d50887f06.jpg' alt='pics'/>
+                    </div>
+                    <div className="userpics">
+                        <img src='https://i.pinimg.com/236x/08/05/2c/08052ca5fd78323538dde509f81d5f15.jpg' alt='pics'/>
+                    </div>
+                    
+                </div>
+            </div>
         </section>
     );
 }
